fc7bd97f8952cd8488037639203647d7f9d0577f
John Molakvoæ <skjnldsv@protonmail.com>
2022-07-01 17:29:20 +0200

    Better logging messages and container cleanup
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- cypress/dockerNode.ts
+++ cypress/dockerNode.ts
@@ -3,17 +3,16 @@ import * as Docker from 'dockerode'
 const docker = new Docker()
 const CONTAINER_NAME = 'nextcloud-cypress-tests'
 
+// TODO: use branch ?
 export const startNextcloud = async function(branch = 'master') {
 	try {
-		await docker.version().then(console.log)
-
 		// Remove old container if exists
 		try {
 			const oldContainer = docker.getContainer(CONTAINER_NAME)
 			await oldContainer.remove({ force: true })
 		} catch (error) {}
 
-		console.log('Starting Nextcloud container')
+		console.log('Starting Nextcloud container...')
 		const container = await docker.createContainer(
 			{
 				Image: 'ghcr.io/nextcloud/continuous-integration-shallow-server',
@@ -33,10 +32,10 @@ export const startNextcloud = async function(branch = 'master') {
 			})
 
 			await sleep(1000 * tries)
-			console.log('Waiting for Nextcloud container\'s IP')
+			console.log('> Waiting for Nextcloud container\'s IP...')
 		}
 
-		console.log(`> Nextcloud container's IP is ${ip}`)
+		console.log(`> Nextcloud container's IP is ${ip} 🌏`)
 		return ip
 	} catch(err) {
 		console.log(err)
@@ -44,19 +43,19 @@ export const startNextcloud = async function(branch = 'master') {
 }
 
 export const configureNextcloud = async function() {
-	console.log('Configuring nextcloud')
+	console.log('Configuring nextcloud...')
 	const container = docker.getContainer(CONTAINER_NAME)
 	runExec(container, ['php', 'occ', 'config:system:set', 'force_language', '--value', 'en_US'])
 	runExec(container, ['php', 'occ', 'config:system:set', 'enforce_theme', '--value', 'light'])
+	console.log('> Nextcloud is now ready to use 🎉')
 }
 
 export const stopNextcloud = async function() {
 	try {
 		const container = docker.getContainer(CONTAINER_NAME)
-		console.log('Stopping Nextcloud container')
-		await container.stop()
-		await container.remove({ force: true })
-		console.log('Nextcloud container removed')
+		console.log('Stopping Nextcloud container...')
+		container.remove({ force: true })
+		console.log('> Nextcloud container removed 🥀')
 	} catch(err) {
 		console.log(err)
 	}
