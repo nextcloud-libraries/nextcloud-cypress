57328cf7fa271f11b171a6309c59e00a798138bf
Max <max@nextcloud.com>
2025-02-11 06:18:32 +0100

    test: docker tooling with playwright
    Signed-off-by: Max <max@nextcloud.com>

---
+++ playwright/e2e/random-user.spec.ts
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ferdinand Thiessen <opensource@fthiessen.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { expect } from '@playwright/test'
+import { test } from '../support/fixtures/random-user'
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('apps/files')
+	await page.waitForURL(/apps\/files/)
+})
+
+test('Random user is authenticated', async ({ page }) => {
+	await expect(page.getByLabel('Settings menu')).toBeVisible()
+})
